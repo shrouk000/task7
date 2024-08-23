a44809930745a3e3c90ee5f8a9bafccf5824250f
shrouk000 <shroukshams456@gmail.com>
2024-08-24 01:46:06 +0300

    Add files via upload

---
+++ sidebar.blade.php
@@ -0,0 +1,26 @@
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+						<div class="menu_section">
+							<h3>General</h3>
+							<ul class="nav side-menu">
+								<li><a><i class="fa fa-users"></i> Users <span class="fa fa-chevron-down"></span></a>
+									<ul class="nav child_menu">
+										<li><a href="users.html">Users List</a></li>
+										<li><a href="addUser.html">Add User</a></li>
+									</ul>
+								</li>
+								<li><a><i class="fa fa-edit"></i> Tags <span class="fa fa-chevron-down"></span></a>
+									<ul class="nav child_menu">
+										<li><a href="addCategory.html">Add Tag</a></li>
+										<li><a href="categories.html">Tags List</a></li>
+									</ul>
+								</li>
+								<li><a><i class="fa fa-desktop"></i> Photos <span class="fa fa-chevron-down"></span></a>
+									<ul class="nav child_menu">
+										<li><a href="addPhoto.html">Add Photo</a></li>
+										<li><a href="photos.html">Photos List</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+
+					</div>
